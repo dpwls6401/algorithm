d89494ef1243e3bc190ebc52adb0364a3317f95e
dpwls6401 <dpwls0144@naver.com>
2022-02-23 10:42:07 +0900

    al-2

--- BOJ_1100.py
+++ BOJ_1100.py
@@ -1,16 +1,18 @@
-chess = [list(input()) for i in range(8)]
+# 하얀 칸 - 2차원 리스트 응용
 
 # 1. without comprehension
-# white_chess = []
-# for i in range(8):
-#     for j in range(8):
-#         if (i % 2 == j % 2) and chess[i][j] == "F":
-#             white_chess.append(chess[i][j])
-# print(len(white_chess))
+chess = [list(input().split("")) for i in range(8)]
+white_chess = []
+for i in range(8):
+    for j in range(8):
+        if (i % 2 == j % 2) and chess[i][j] == "F":
+            white_chess.append(chess[i][j])
+print(len(white_chess))
 
 # # 2. w/ list comprehension
-# white_chess = [chess[i][j] for i in range(8) for j in range(8) if (i % 2 == j % 2) and (chess[i][j] == "F")]
-# print(len(white_chess))
+chess = [list(input().split("")) for i in range(8)]
+white_chess = [chess[i][j] for i in range(8) for j in range(8) if (i % 2 == j % 2) and (chess[i][j] == "F")]
+print(len(white_chess))
 
 # without comprehension - 피드백 반영
 chess = [list(input()) for i in range(8)]
